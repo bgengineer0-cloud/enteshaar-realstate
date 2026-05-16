3c84d3ad70daa7edec048916a6675a2f87adfabc
soliman <bgengineer0@gmail.com>
2026-05-16 04:11:32 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,18 @@ import { ContentProvider } from './context/ContentContext';
 import Website from './pages/Website';
 import Dashboard from './pages/Dashboard';
 import './App.css';
-
+import About from "./pages/About";
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 export default function App() {
   return (
     <ContentProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Website />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Contact" element={<Contact />} />
           <Route path="/admin/*" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
